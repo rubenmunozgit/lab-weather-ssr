b663099aeb74de7c33b2d613621d450abe540e68
rpalomino <rpalomino@vrbo.com>
2021-05-26 13:04:27 +0200

    prep for heroku

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,22 @@
+import path from 'path';
 import express from 'express';
 import compression from 'compression';
 const exphbs = require('express-handlebars');
 import router from './routes';
-import errorHandler from './errorHandler'
-
+import errorHandler from './errorHandler';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.engine('.hbs', exphbs({ extname: '.hbs' }));
-app.set('views', './build/views');
+app.set('views', path.join(process.cwd(), 'build', 'views'));
 app.set('view engine', '.hbs');
 app.set('trust proxy', true);
 
 app.use(compression());
 app.use('/', router);
 app.use(errorHandler);
-app.use(express.static('build'));
+app.use(express.static(path.join(process.cwd(), 'build')));
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
